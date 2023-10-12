fc71e1ca611c2c9a005a96110c53da06fe0f4b3c
Denis-Baranov <denizzka@tut.by>
2023-10-12 18:53:50 +0300

    refactor Project

--- Project.java
+++ Project.java
@@ -1,10 +1,11 @@
-public class Project{
+public class Project {
 
     public static void main(String[] args) {
 
-        int a=3;
-        int b=4;
-        int c = a+b;
-        System.out.println(c);
+        int a = 3;
+        int b = 4;
+        int c = a + b;
+        System.out.println(a);
+        System.out.println(b);
     }
 }
